16d21161d58a16ab292a58efbc1acf32ff22b940
Olga <marinchak_o@mail.ru>
2023-09-07 21:08:56 +0300

    Добавлены задачи Task064,Task066, Task068

---
+++ Task066/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное число 1: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число 2: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    Console.WriteLine("Это не натуральное число. Введите натуральное число (больше 0)");
+    return;
+}
+
+int SumNumbers(int numberM, int numberN)
+{
+    if (numberM == numberN) 
+    {
+        return numberN;
+    }  
+    else if (numberM < numberN)
+    {
+        return numberN + SumNumbers(numberM, numberN - 1);
+    }
+    else 
+    {
+        return numberN + SumNumbers(numberM, numberN + 1);
+    }
+}
+
+Console.Write($"\nM = {numberM}; N = {numberN} - >");
+Console.Write(SumNumbers(numberM, numberN));
+
---
+++ Task068/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.WriteLine("Введите первое число N больше 0, но меньше 4: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число M больше 0, но меньше 11: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if(numberM < 0 || numberN < 0) 
+{
+    Console.WriteLine("Некорректный ввод. Введите натуральное число (больше 0)");
+    return;
+}
+
+int Akkerman(int numberM, int numberN)
+{
+    if (numberM == 0)
+        return numberN + 1;
+    if (numberM > 0 && numberN == 0)
+        return Akkerman(numberM - 1, 1);
+    else
+        return Akkerman(numberM - 1, Akkerman(numberM, numberN - 1));
+}
+
+int InputNumber(string message)
+{
+ Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"A({numberM}, {numberN}) = {Akkerman(numberM, numberN)}");
